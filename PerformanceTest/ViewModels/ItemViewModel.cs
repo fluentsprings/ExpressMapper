d6c81acce036913acce4f19c3ee72193eac8d8fd
bogatykh <bogatykh@gmail.com>
2015-06-23 10:55:09 +0300

    Performance test fixed.

--- PerformanceTest/ViewModels/ItemViewModel.cs
+++ PerformanceTest/ViewModels/ItemViewModel.cs
@@ -2,7 +2,7 @@
 
 namespace PerformanceTest.ViewModels
 {
-    public struct ItemViewModel
+    public class ItemViewModel
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
