801108421f9015e1b49d8a6b6eb013659bb16f49
bogatykh <bogatykh@gmail.com>
2015-07-17 09:35:47 +0300

    Null List to Array map.

--- ExpressMapper.Tests NET40/BasicTests.cs
+++ ExpressMapper.Tests NET40/BasicTests.cs
@@ -351,6 +351,29 @@ namespace ExpressMapper.Tests
         }
 
         [Test]
+        public void ListNullToArray()
+        {
+            Mapper.Register<TestCollection, TestCollectionViewModel>();
+            Mapper.Register<TestItem, TestItemViewModel>()
+                .Member(dest => dest.Array, src => src.List)
+                .Ignore(dest => dest.Collection)
+                .Ignore(dest => dest.Enumerable)
+                .Ignore(dest => dest.List)
+                .Ignore(dest => dest.Queryable);
+
+            Mapper.Compile();
+
+            var typeCollTest = new TestItem()
+            {
+                List = null
+            };
+
+            var result = Mapper.Map<TestItem, TestItemViewModel>(typeCollTest);
+
+            Assert.IsNull(result.Array);
+        }
+
+        [Test]
         public void ListToQueriable()
         {
             Mapper.Register<TestCollection, TestCollectionViewModel>();
