529367dcff2c1ba741896f8ccaa316067f4bda56
anisimovyuriy <anisimov.yuriy@gmail.com>
2015-07-18 07:11:45 -0700

    Direct collection mappings Mapper.Map<List<TSrc>, TDest[]> are not precompiled #29

---
+++ Benchmarks/ViewModels/ItemViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Benchmarks.ViewModels
+{
+    public class ItemViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public decimal Weight { get; set; }
+        public decimal Length { get; set; }
+        public decimal Width { get; set; }
+        public decimal Height { get; set; }
+    }
+}
